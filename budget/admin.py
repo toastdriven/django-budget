52f30e498be00f4b563d7e35fa070b3a7409e0c2
Daniel Lindsley <dlindsley@mediaphormedia.com>
2008-10-18 21:40:22 -0500

    Customized admins.

--- budget/admin.py
+++ budget/admin.py
@@ -1,5 +1,39 @@
 from django.contrib import admin
 from budget.models import Budget, BudgetEstimate
 
-admin.site.register(Budget)
-admin.site.register(BudgetEstimate)
+
+class BudgetAdmin(admin.ModelAdmin):
+    date_hierarchy = 'start_date'
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'slug', 'start_date'),
+        }),
+        ('Metadata', {
+            'classes': ('collapse',),
+            'fields': ('created', 'updated', 'is_deleted')
+        })
+    )
+    list_display = ('name', 'start_date', 'is_deleted')
+    list_filter = ('is_deleted',)
+    prepopulated_fields = {
+        'slug': ('name',),
+    }
+    search_fields = ('name',)
+
+
+class BudgetEstimateAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': ('budget', 'category', 'amount'),
+        }),
+        ('Metadata', {
+            'classes': ('collapse',),
+            'fields': ('created', 'updated', 'is_deleted')
+        })
+    )
+    list_display = ('category', 'budget', 'amount', 'is_deleted')
+    list_filter = ('is_deleted',)
+
+
+admin.site.register(Budget, BudgetAdmin)
+admin.site.register(BudgetEstimate, BudgetEstimateAdmin)
