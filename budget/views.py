73a77dbefade5575754fcf3d884dbba679551502
mpeterson <michel@peterson.com.ar>
2012-11-04 17:17:39 -0300

    Added division by 0 handling to the Dashboard view

--- budget/views.py
+++ budget/views.py
@@ -16,7 +16,7 @@ from budget.forms import BudgetEstimateForm, BudgetForm
 def dashboard(request, budget_model_class=Budget, transaction_model_class=Transaction, template_name='budget/dashboard.html'):
     """
     Provides a high-level rundown of recent activity and budget status.
-    
+
     Template: ``budget/dashboard.html``
     Context:
         budget
@@ -35,30 +35,34 @@ def dashboard(request, budget_model_class=Budget, transaction_model_class=Transa
     today = datetime.date.today()
     start_date = datetime.date(today.year, today.month, 1)
     end_year, end_month = today.year, today.month + 1
-    
+
     if end_month > 12:
         end_year += 1
         end_month = 1
-    
+
     end_date = datetime.date(end_year, end_month, 1) - datetime.timedelta(days=1)
-    
+
     try:
         budget = budget_model_class.active.most_current_for_date(datetime.datetime.today())
     except ObjectDoesNotExist:
         # Since there are no budgets at this point, pass them on to setup
         # as this view is meaningless without at least basic data in place.
         return HttpResponseRedirect(reverse('budget_setup'))
-    
+
     latest_expenses = transaction_model_class.expenses.get_latest()
     latest_incomes = transaction_model_class.incomes.get_latest()
-    
+
     estimated_amount = budget.monthly_estimated_total()
     amount_used = budget.actual_total(start_date, end_date)
-    progress_bar_percent = int(amount_used / estimated_amount * 100)
-    
+
+    if estimated_amount == 0:
+        progress_bar_percent = 100
+    else:
+        progress_bar_percent = int(amount_used / estimated_amount * 100)
+
     if progress_bar_percent >= 100:
         progress_bar_percent = 100
-    
+
     return render_to_response(template_name, {
         'budget': budget,
         'latest_expenses': latest_expenses,
@@ -74,7 +78,7 @@ def setup(request, template_name='budget/setup.html'):
     Displays a setup page which ties together the
     category/budget/budget estimate areas with explanatory text on how to use
     to set everything up properly.
-    
+
     Templates: ``budget/setup.html``
     """
     return render_to_response(template_name, {}, context_instance=RequestContext(request))
@@ -83,7 +87,7 @@ def setup(request, template_name='budget/setup.html'):
 def summary_list(request, transaction_model_class=Transaction, template_name='budget/summaries/summary_list.html'):
     """
     Displays a list of all months that may have transactions for that month.
-    
+
     Templates: ``budget/summaries/summary_list.html``
     Context:
         dates
@@ -98,7 +102,7 @@ def summary_list(request, transaction_model_class=Transaction, template_name='bu
 def summary_year(request, year, budget_model_class=Budget, template_name='budget/summaries/summary_year.html'):
     """
     Displays a budget report for the year to date.
-    
+
     Templates: ``budget/summaries/summary_year.html``
     Context:
         budget
@@ -128,7 +132,7 @@ def summary_year(request, year, budget_model_class=Budget, template_name='budget
 def summary_month(request, year, month, budget_model_class=Budget, template_name='budget/summaries/summary_month.html'):
     """
     Displays a budget report for the month to date.
-    
+
     Templates: ``budget/summaries/summary_month.html``
     Context:
         budget
@@ -144,11 +148,11 @@ def summary_month(request, year, month, budget_model_class=Budget, template_name
     """
     start_date = datetime.date(int(year), int(month), 1)
     end_year, end_month = int(year), int(month) + 1
-    
+
     if end_month > 12:
         end_year += 1
         end_month = 1
-    
+
     end_date = datetime.date(end_year, end_month, 1) - datetime.timedelta(days=1)
     budget = budget_model_class.active.most_current_for_date(end_date)
     estimates_and_transactions, actual_total = budget.estimates_and_transactions(start_date, end_date)
@@ -199,7 +203,7 @@ def budget_add(request, form_class=BudgetForm, template_name='budget/budgets/add
     """
     if request.POST:
         form = form_class(request.POST)
-        
+
         if form.is_valid():
             budget = form.save()
             return HttpResponseRedirect(reverse('budget_budget_list'))
@@ -224,7 +228,7 @@ def budget_edit(request, slug, model_class=Budget, form_class=BudgetForm, templa
     budget = get_object_or_404(model_class.active.all(), slug=slug)
     if request.POST:
         form = form_class(request.POST, instance=budget)
-        
+
         if form.is_valid():
             category = form.save()
             return HttpResponseRedirect(reverse('budget_budget_list'))
@@ -300,7 +304,7 @@ def estimate_add(request, budget_slug, budget_model_class=Budget, form_class=Bud
     budget = get_object_or_404(budget_model_class.active.all(), slug=budget_slug)
     if request.POST:
         form = form_class(request.POST)
-        
+
         if form.is_valid():
             estimate = form.save(budget=budget)
             return HttpResponseRedirect(reverse('budget_estimate_list', kwargs={'budget_slug': budget.slug}))
@@ -332,7 +336,7 @@ def estimate_edit(request, budget_slug, estimate_id, budget_model_class=Budget, 
         raise Http404("The requested estimate could not be found.")
     if request.POST:
         form = form_class(request.POST, instance=estimate)
-        
+
         if form.is_valid():
             category = form.save(budget=budget)
             return HttpResponseRedirect(reverse('budget_estimate_list', kwargs={'budget_slug': budget.slug}))
